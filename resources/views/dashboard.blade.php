62cc7c43514e3fe1f81386c7b9c7bef1088b7074
Angelo <angelofestino5@gmail.com>
2024-05-11 02:49:01 -0300

    Cambio de icono cuenta

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -1,6 +1,6 @@
 <x-app-layout>
     <x-slot name="header">
-        <h2 class="font-semibold text-xl text-gray-800 leading-tight tituloPantalla">
+        <h2 class="font-semibold text-xl text-gray-800 leading-tight text-center">
             {{ __('Inicio') }}
         </h2>
     </x-slot>
--- resources/views/layouts/navigation.blade.php
+++ resources/views/layouts/navigation.blade.php
@@ -79,12 +79,11 @@
                     <a class="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown"
                         aria-expanded="false">
                         <svg xmlns="http://www.w3.org/2000/svg" width="30" height="30" fill="currentColor"
-                            class="bi bi-person-circle m-2 {{ Auth::user()->hasRole('admin_Liga') ? 'text-danger' : (Auth::user()->hasRole('admin_Equipo') ? 'text-warning' : 'text-info') }}"
+                            class="bi bi-person-check-fill {{ Auth::user()->hasRole('admin_Liga') ? 'text-danger' : (Auth::user()->hasRole('admin_Equipo') ? 'text-warning' : 'text-info') }}"
                             viewBox="0 0 16 16">
-
-                            <path d="M11 6a3 3 0 1 1-6 0 3 3 0 0 1 6 0" />
                             <path fill-rule="evenodd"
-                                d="M0 8a8 8 0 1 1 16 0A8 8 0 0 1 0 8m8-7a7 7 0 0 0-5.468 11.37C3.242 11.226 4.805 10 8 10s4.757 1.225 5.468 2.37A7 7 0 0 0 8 1" />
+                                d="M15.854 5.146a.5.5 0 0 1 0 .708l-3 3a.5.5 0 0 1-.708 0l-1.5-1.5a.5.5 0 0 1 .708-.708L12.5 7.793l2.646-2.647a.5.5 0 0 1 .708 0" />
+                            <path d="M1 14s-1 0-1-1 1-4 6-4 6 3 6 4-1 1-1 1zm5-6a3 3 0 1 0 0-6 3 3 0 0 0 0 6" />
                         </svg>
                         {{ Auth::user()->name }}
                     </a>
