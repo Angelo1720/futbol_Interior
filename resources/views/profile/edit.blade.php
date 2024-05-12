4b915ce0b573ab993463cb06ec5d85822aea7da7
Angelo <angelofestino5@gmail.com>
2024-05-11 21:23:46 -0300

    [miPerfil] mejoras en visual

--- resources/views/profile/edit.blade.php
+++ resources/views/profile/edit.blade.php
@@ -7,19 +7,19 @@
 
     <div class="py-12">
         <div class="max-w-7xl mx-auto sm:px-6 lg:px-8 space-y-6">
-            <div class="p-4 sm:p-8 bg-white shadow sm:rounded-lg">
+            <div class="p-4 sm:p-8 bg-primary-subtle shadow sm:rounded-lg divActualizarContraseña">
                 <div class="max-w-xl">
                     @include('profile.partials.update-profile-information-form')
                 </div>
             </div>
 
-            <div class="p-4 sm:p-8 bg-white shadow sm:rounded-lg">
+            <div class="p-4 sm:p-8 bg-primary-subtle shadow sm:rounded-lg divActualizarContraseña">
                 <div class="max-w-xl">
                     @include('profile.partials.update-password-form')
                 </div>
             </div>
 
-            <div class="p-4 sm:p-8 bg-white shadow sm:rounded-lg">
+            <div class="p-4 sm:p-8 bg-danger-subtle shadow sm:rounded-lg divBorrarCuenta">
                 <div class="max-w-xl">
                     @include('profile.partials.delete-user-form')
                 </div>
