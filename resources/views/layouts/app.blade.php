fbbc88301fe3e53002e1639a9271243918e5dba0
Franco Sancristóbal <franco.sancris29@gmail.com>
2024-05-29 17:59:02 -0300

    fix tablas

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -25,8 +25,7 @@
 
     <!-- Bootstrap CSS -->
     <link href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css" rel="stylesheet">
-    <!-- Incluir jQuery -->
-    <script src="https://code.jquery.com/jquery-3.5.1.min.js"></script>
+    
     <!-- Incluir Popper.js -->
     <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.9.2/dist/umd/popper.min.js"></script>
     <!-- Incluir Bootstrap JS -->
