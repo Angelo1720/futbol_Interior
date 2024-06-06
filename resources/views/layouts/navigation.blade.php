748568d0deab35ddcc119e68054f589e3a0292bd
Angelo <angelofestino5@gmail.com>
2024-06-06 02:22:20 -0300

    botones de Login y Registro a la izquierda falta css

--- resources/views/layouts/navigation.blade.php
+++ resources/views/layouts/navigation.blade.php
@@ -12,9 +12,8 @@
                             alt="Icono"> Comunidad</a>
                 </li>
                 <li class="nav-item dropdown">
-                    <a class="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown"
-                        aria-expanded="false"> <img src="/Images/copa-de-futbol.png" alt="Icono">
-                        Campeonatos
+                    <a class="nav-link" href="#" role="button">
+                        <img src="/Images/copa-de-futbol.png" alt="Icono"> Campeonatos
                     </a>
                     <ul class="dropdown-menu">
                         <li><a class="dropdown-item" href="#">Divisional "A"</a></li>
@@ -23,9 +22,8 @@
                     </ul>
                 </li>
                 <li class="nav-item dropdown">
-                    <a class="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown"
-                        aria-expanded="false"> <img src="/Images/club-de-futbol.png" alt="Icono">
-                        Clubes
+                    <a class="nav-link" href="#" role="button">
+                        <img src="/Images/club-de-futbol.png" alt="Icono"> Clubes
                     </a>
                     <ul class="dropdown-menu">
                         <li><a class="dropdown-item" href="#">Títulos</a></li>
@@ -33,8 +31,7 @@
                     </ul>
                 </li>
                 <li class="nav-item dropdown">
-                    <a class="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown"
-                        aria-expanded="false">
+                    <a class="nav-link" href="#" role="button">
                         <img src="/Images/uruguay.png" alt="Icono"> Departamento
                     </a>
                     <ul class="dropdown-menu">
@@ -46,9 +43,8 @@
                 @auth
                     @role('admin_Liga')
                         <li class="nav-item dropdown">
-                            <a class="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown"
-                                aria-expanded="false"> <img src="/Images/administracion.png" alt="Icono">
-                                Administrar
+                            <a class="nav-link" href="#" role="button">
+                                <img src="/Images/administracion.png" alt="Icono"> Administrar
                             </a>
                             <ul class="dropdown-menu">
                                 <li><a class="dropdown-item" href="{{ route('usuarios') }}">Usuarios</a></li>
@@ -62,8 +58,7 @@
                     @endrole
                     @role('admin_Equipo')
                         <li class="nav-item dropdown">
-                            <a class="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown"
-                                aria-expanded="false">
+                            <a class="nav-link dropdown-toggle" href="#" role="button">
                                 Administrar
                             </a>
                             <ul class="dropdown-menu">
@@ -72,14 +67,11 @@
                             </ul>
                         </li>
                     @endrole
-                
-            </ul>
-        </div>
-        <div>
+                </ul>
+            </div>
             <ul class="navbar-nav">
                 <li class="nav-item dropdown">
-                    <a class="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown"
-                        aria-expanded="false">
+                    <a class="nav-link dropdown-toggle" href="#" role="button">
                         <svg xmlns="http://www.w3.org/2000/svg" width="30" height="30" fill="currentColor"
                             class="bi bi-person-check-fill {{ Auth::user()->hasRole('admin_Liga') ? 'text-danger' : (Auth::user()->hasRole('admin_Equipo') ? 'text-warning' : 'text-info') }}"
                             viewBox="0 0 16 16">
@@ -90,22 +82,28 @@
                         {{ Auth::user()->name }}
                     </a>
                     <ul class="dropdown-menu">
-                        <a class="dropdown-item" href="{{ route('profile.edit') }}"> <img
-                                src="/Images/red-social.png" alt="Icono"> {{ __('Mi perfil') }}</a>
+                        <a class="dropdown-item" href="{{ route('profile.edit') }}"> <img src="/Images/red-social.png"
+                                alt="Icono"> {{ __('Mi perfil') }}</a>
                         <form method="POST" action="{{ route('logout') }}">
                             @csrf
-                            <button type="submit" class="dropdown-item"> <img src="/Images/apagar.png"
-                                    alt="Icono"> {{ __('Cerrar Sesión') }}</button>
+                            <button type="submit" class="dropdown-item"> <img src="/Images/apagar.png" alt="Icono">
+                                {{ __('Cerrar Sesión') }}</button>
                         </form>
                     </ul>
                 </li>
-                @endauth
-                @guest
-                <li class="nav-item">
-                    <a class="nav-link align-content-end" aria-current="page" href="{{route('login')}}">Iniciar Sesión</a>
-                </li>
-                @endguest
+            @endauth
+            @guest
+            </ul class="navbar-nav me-auto mb-2 mb-lg-0">
+            <li class="nav-item">
+                <a class="nav-link" aria-current="page" href="{{ route('login') }}">Iniciar
+                    Sesión</a>
+            </li>
+            <li class="nav-item">
+                <a class="nav-link " aria-current="page" href="{{ route('register') }}">Registrarse</a>
+            </li>
             </ul>
-        </div>
+        @endguest
+        </ul>
+    </div>
     </div>
 </nav>
