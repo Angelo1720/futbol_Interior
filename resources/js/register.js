869e9a91bf4f979ac3d734c09d5bc73783bf3fea
Franco Sancristobal <franco.sancris29@gmail.com>
2024-06-07 10:44:10 -0300

    [fixVarios] fix togglePassword err in console

--- resources/js/register.js
+++ resources/js/register.js
@@ -1,9 +1,10 @@
 document.addEventListener('DOMContentLoaded', function() {
     const togglePassword = document.getElementById('togglePassword');
     const passwordInput = document.getElementById('password');
-
-    togglePassword.addEventListener('click', function() {
-        const type = passwordInput.getAttribute('type') === 'password' ? 'text' : 'password';
-        passwordInput.setAttribute('type', type);
-    });
+    if (togglePassword) {
+        togglePassword.addEventListener('click', function() {
+            const type = passwordInput.getAttribute('type') === 'password' ? 'text' : 'password';
+            passwordInput.setAttribute('type', type);
+        });
+    }
 });
