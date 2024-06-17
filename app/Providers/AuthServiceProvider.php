7ae3c6554af6245111415dead4d3b1c1aba50bd1
Franco Sancristobal <franco.sancris29@gmail.com>
2024-06-17 15:03:36 -0300

    [api] commit para cambiar rama

--- app/Providers/AuthServiceProvider.php
+++ app/Providers/AuthServiceProvider.php
@@ -3,7 +3,10 @@
 namespace App\Providers;
 
 // use Illuminate\Support\Facades\Gate;
+
+use App\Models\ClientToken;
 use Illuminate\Foundation\Support\Providers\AuthServiceProvider as ServiceProvider;
+use Laravel\Sanctum\Sanctum;
 
 class AuthServiceProvider extends ServiceProvider
 {
@@ -24,7 +27,7 @@ class AuthServiceProvider extends ServiceProvider
     public function boot()
     {
         $this->registerPolicies();
-
+        Sanctum::usePersonalAccessTokenModel(ClientToken::class);
         //
     }
 }
