15b64d6c0cd93dc4d5eb1037341ca8569f8ee6cc
Franco Sancristobal <franco.sancris29@gmail.com>
2024-06-24 19:21:58 -0300

    [api] delete clientToken

--- app/Providers/AuthServiceProvider.php
+++ app/Providers/AuthServiceProvider.php
@@ -3,7 +3,6 @@
 namespace App\Providers;
 
 // use Illuminate\Support\Facades\Gate;
-
 use Illuminate\Foundation\Support\Providers\AuthServiceProvider as ServiceProvider;
 use Laravel\Sanctum\Sanctum;
 
