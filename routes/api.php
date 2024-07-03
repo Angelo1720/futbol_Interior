d8849188049c178df78ba72db9d92f9b9cb84f95
Franco Sancristobal <franco.sancris29@gmail.com>
2024-07-03 02:35:19 -0300

    [apiListadoCampeonatos] listado

--- routes/api.php
+++ routes/api.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\CampeonatoController;
 use App\Http\Controllers\EquipoController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
@@ -16,3 +17,4 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/listadoEquipos', [EquipoController::class, 'apiListadoEquipos']);
+Route::get('/listadoCampeonatos', [CampeonatoController::class, 'apiListadoCampeonatos']);
